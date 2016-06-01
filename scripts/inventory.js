2b541e7e2c15f89b78fef27d429adfce06c0038c
devjanaprime <dev@primeacademy.io>
2016-06-01 10:40:57 -0500

    added calculations

--- scripts/inventory.js
+++ scripts/inventory.js
@@ -6,11 +6,17 @@ var getInput = function(){
   var capacity = document.getElementById('capacityIn').value;
   var fill = document.getElementById('fillIn').value;
 
+  var currentCapacity = capacity * fill;
+  var fixedCurrentCapacity = currentCapacity.toFixed(2);
+  console.log( name + "'s current capacity: " + fixedCurrentCapacity );
+
   var newContainer ={
     'name': name,
     'capacity': capacity,
     'fill': fill
   };
+
+
   containers.push( newContainer );
 
   console.log( containers );
