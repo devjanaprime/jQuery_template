f3e28816e2a241497c9eac52f15750d024d34760
devjanaprime <dev@primeacademy.io>
2016-06-01 11:15:41 -0500

    updated to jQuery

--- scripts/inventory.js
+++ scripts/inventory.js
@@ -1,23 +1,20 @@
 var containers = [];
 
-var getInput = function(){
-  console.log( 'in getInput' );
-  var name = document.getElementById('containerNameIn').value;
-  var capacity = document.getElementById('capacityIn').value;
-  var fill = document.getElementById('fillIn').value;
+$(document).ready(function(){
 
-  var currentCapacity = capacity * fill;
-  var fixedCurrentCapacity = currentCapacity.toFixed(2);
-  console.log( name + "'s current capacity: " + fixedCurrentCapacity );
-
-  var newContainer ={
-    'name': name,
-    'capacity': capacity,
-    'fill': fill
-  };
-
-
-  containers.push( newContainer );
-
-  console.log( containers );
-}
+  $('#addContainerButton').click(function(){
+    var name = $('#containerNameIn').val();
+    var capacity = $('#capacityIn').val();
+    var fill = $('#fillIn').val();
+    var currentCapacity = capacity * fill;
+    var fixedCurrentCapacity = currentCapacity.toFixed(2);
+    console.log( name + "'s current capacity: " + fixedCurrentCapacity );
+    var newContainer ={
+      'name': name,
+      'capacity': capacity,
+      'fill': fill
+    };
+    containers.push( newContainer );
+    console.log( containers );
+  });
+});
