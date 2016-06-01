345bcd768d6e729b45565136ba65da5a3d8efa66
devjanaprime <dev@primeacademy.io>
2016-06-01 10:35:32 -0500

    updated inventory.js

--- scripts/inventory.js
+++ scripts/inventory.js
@@ -1,8 +1,17 @@
+var containers = [];
+
 var getInput = function(){
   console.log( 'in getInput' );
   var name = document.getElementById('containerNameIn').value;
   var capacity = document.getElementById('capacityIn').value;
   var fill = document.getElementById('fillIn').value;
 
-  console.log( 'name: ' + name + 'capacity: ' + capacity + ' fill: ' + fill );
+  var newContainer ={
+    'name': name,
+    'capacity': capacity,
+    'fill': fill
+  };
+  containers.push( newContainer );
+
+  console.log( containers );
 }
