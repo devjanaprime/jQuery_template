3f0009f3ac72f7dc87422fdc83b9b80b377d6928
devjanaprime <dev@primeacademy.io>
2016-06-01 10:31:32 -0500

    renamed js file and captred user input

---
+++ scripts/inventory.js
@@ -0,0 +1,8 @@
+var getInput = function(){
+  console.log( 'in getInput' );
+  var name = document.getElementById('containerNameIn').value;
+  var capacity = document.getElementById('capacityIn').value;
+  var fill = document.getElementById('fillIn').value;
+
+  console.log( 'name: ' + name + 'capacity: ' + capacity + ' fill: ' + fill );
+}
